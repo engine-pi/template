07190a053be427afd5b9d17630699172732f861b
Josef Friedrich <josef@friedrich.rocks>
2025-11-16 21:08:11 +0100

    Add src folder

---
+++ src/main/java/de/pirckheimer_gymnasium/project/Main.java
@@ -0,0 +1,11 @@
+package de.pirckheimer_gymnasium.project;
+
+import de.pirckheimer_gymnasium.engine_pi.Game;
+
+public class Main
+{
+    public static void main(String[] args)
+    {
+        Game.start();
+    }
+}
